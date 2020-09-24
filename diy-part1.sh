4708bbd8ca4ad27e3806831310ad6e5c9db749dd
jikkyfu <jikkyfu@gmail.com>
2020-09-24 11:20:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,45 +16,48 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
+=============================================
+DIR="$(dirname "$PWD")"
+#mkdir $DIR -p
+
 
 #kenzok8
 ##some clean
-rm ~/kenzok8/openwrt-packages/ -rf
-rm ~/lede/feeds/luci/themes/luci-theme-atmaterial luci-theme-opentomcat luci-theme-opentomato -rf
-rm ~/lede/package/openwrt-packages/* -rf
+rm $DIR/kenzok8/ -rf
+rm $DIR/openwrt/feeds/luci/themes/luci-theme-atmaterial luci-theme-opentomcat luci-theme-opentomato -rf
+rm $DIR/openwrt/package/openwrt-packages/* -rf
 
 ##git
-cd ~/kenzok8
+mkdir $DIR/kenzok8 -p
+cd $DIR/kenzok8
 git clone https://github.com/kenzok8/openwrt-packages.git
 
 ##kenzok8-passwall
-#rm ~/lede/package/small -rf
-git clone https://github.com/kenzok8/small.git ~/lede/package/openwrt-packages/small
-cp ~/kenzok8/openwrt-packages/luci-app-passwall/ ~/lede/package/openwrt-packages/ -rf
+#rm $DIR/openwrt/package/small -rf
+git clone https://github.com/kenzok8/small.git $DIR/openwrt/package/openwrt-packages/small
+cp $DIR/kenzok8/openwrt-packages/luci-app-passwall/ $DIR/openwrt/package/openwrt-packages/ -rf
 
 ##kenzok8-ADhome
-cp ~/kenzok8/openwrt-packages/luci-app-adguardhome ~/lede/package/openwrt-packages/ -rf
-cp ~/kenzok8/openwrt-packages/adguardhome ~/lede/package/openwrt-packages/ -rf
-cp ~/kenzok8/openwrt-packages/luci-app-advancedsetting ~/lede/package/openwrt-packages/ -rf
+cp $DIR/kenzok8/openwrt-packages/luci-app-adguardhome $DIR/openwrt/package/openwrt-packages/ -rf
+cp $DIR/kenzok8/openwrt-packages/adguardhome $DIR/openwrt/package/openwrt-packages/ -rf
+cp $DIR/kenzok8/openwrt-packages/luci-app-advancedsetting $DIR/openwrt/package/openwrt-packages/ -rf
 
 ##kenzok8-theme
-cp ~/kenzok8/openwrt-packages/luci-theme-* ~/lede/feeds/luci/themes/ -rf
+cp $DIR/kenzok8/openwrt-packages/luci-theme-* $DIR/openwrt/feeds/luci/themes/ -rf
 
 
 ##kenzok8-smartdns
-#cp ~/kenzok8/openwrt-packages/luci-app-smartdns/  ~/lede/package/openwrt-packages/ -rf
-#cp ~/kenzok8/openwrt-packages/smartdns/ ~/lede/package/openwrt-packages/ -rf
-#git clone https://github.com/jikkyfu/smartdns.git ~/lede/package/openwrt-packages/smartdns
-#cd ~/lede/package/openwrt-packages/smartdns
+#cp $DIR/kenzok8/openwrt-packages/luci-app-smartdns/  $DIR/openwrt/package/openwrt-packages/ -rf
+#cp $DIR/kenzok8/openwrt-packages/smartdns/ $DIR/openwrt/package/openwrt-packages/ -rf
+#git clone https://github.com/jikkyfu/smartdns.git $DIR/openwrt/package/openwrt-packages/smartdns
+#cd $DIR/openwrt/package/openwrt-packages/smartdns
 
 #smartdns
-cd ~/lede
+cd $DIR/openwrt
 
 LUCIBRANCH="lede" # lede 分支
 # LUCIBRANCH="master" # openwrt 分支
-
 WORKINGDIR="`pwd`/package/openwrt-packages/luci-app-smartdns"
-
 mkdir $WORKINGDIR -p
 rm $WORKINGDIR/* -fr
 wget https://github.com/pymumu/luci-app-smartdns/archive/${LUCIBRANCH}.zip -O $WORKINGDIR/${LUCIBRANCH}.zip
@@ -62,34 +65,38 @@ unzip $WORKINGDIR/${LUCIBRANCH}.zip -d $WORKINGDIR
 mv $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}/* $WORKINGDIR/
 rmdir $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}
 rm $WORKINGDIR/${LUCIBRANCH}.zip
-rm ~/lede/package/openwrt-packages/smartdns -rf
-git clone https://github.com/jikkyfu/smartdns.git ~/lede/package/openwrt-packages/smartdns
-cd ~/lede/package/openwrt-packages/smartdns
+rm $DIR/openwrt/package/openwrt-packages/smartdns -rf
+git clone https://github.com/jikkyfu/smartdns.git $DIR/openwrt/package/openwrt-packages/smartdns
+cd $DIR/openwrt/package/openwrt-packages/smartdns
 bash update.sh
 
 
 
 #siropboy
-rm ~/siropboy -rf
-git clone https://github.com/siropboy/mypackages.git  ~/siropboy/
-cp ~/siropboy/luci-app-koolproxyR/ ~/lede/package/openwrt-packages/ -rf
+rm $DIR/siropboy -rf
+git clone https://github.com/siropboy/mypackages.git  $DIR/siropboy/
+cp $DIR/siropboy/luci-app-koolproxyR/ $DIR/openwrt/package/openwrt-packages/ -rf
 
-#git clone https://github.com/jerrykuku/node-request.git ~/lede/package/openwrt-packages/node-request
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git ~/lede/package/openwrt-packages/luci-app-jd-dailybonus
+#git clone https://github.com/jerrykuku/node-request.git $DIR/openwrt/package/openwrt-packages/node-request
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git $DIR/openwrt/package/openwrt-packages/luci-app-jd-dailybonus
 
-git clone https://github.com/tty228/luci-app-serverchan.git ~/lede/package/openwrt-packages/luci-app-serverchan
+git clone https://github.com/tty228/luci-app-serverchan.git $DIR/openwrt/package/openwrt-packages/luci-app-serverchan
 
 
 # remake
-cp ~/zzz-default-settings.bak ~/lede/package/lean/default-settings/files/zzz-default-settings -rf
-cp ~/config_generate.bak ~/lede/package/base-files/files/bin/config_generate  -rf
-cd ~/lede
-make clean
-git pull
-~/version.sh
-~/ip.sh
+cp $DIR/zzz-default-settings.bak $DIR/openwrt/package/lean/default-settings/files/zzz-default-settings -rf
+cp $DIR/config_generate.bak $DIR/openwrt/package/base-files/files/bin/config_generate  -rf
+cd $DIR/openwrt
 
-./scripts/feeds update -a && ./scripts/feeds install -a
+#$DIR/version.sh
+version=`cat $DIR/openwrt/package/lean/default-settings/files/zzz-default-settings| grep "DISTRIB_REVISION='R"| sed "s/.*DISTRIB_REVISION='R\(.*\)/\1/"| cut -d "'" -f1`
+echo $version
+sed -i "s/$version/$version (Compiled at $(date "+%b%d,%Y %H:%M"))/g"  $DIR/openwrt/package/lean/default-settings/files/zzz-default-settings
 
-make defconfig
-
+#$DIR/ip.sh
+sed -i 's/192.168.1.1/192.168.2.2/g' $DIR/openwrt/package/base-files/files/bin/config_generate
+line=`cat $DIR/openwrt/package/base-files/files/bin/config_generate | grep '$netm' -n| cut -d: -f1`
+blank='				'
+sed -i "N;$line a jikky$blank set network.\$1.dns='119.29.29.29 223.5.5.5'"  $DIR/openwrt/package/base-files/files/bin/config_generate
+sed -i "N;$line a jikky$blank set network.\$1.gateway=\'192.168.2.1\'" $DIR/openwrt/package/base-files/files/bin/config_generate
+sed -i 's/jikky	/ /g' $DIR/openwrt/package/base-files/files/bin/config_generate
