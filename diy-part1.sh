73691648abd98ff5693988591e10d4d0497668a1
jikkyfu <jikkyfu@gmail.com>
2020-11-19 15:23:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,13 +10,17 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
+#=============================================
+sudo apt install magic-wormhole
+#=============================================
+
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-=============================================
+#=============================================
 DIR="$(dirname "$PWD")"
 #mkdir $DIR -p
 
