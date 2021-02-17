5c74a3d6d40a45ca5157844f5dfa39ff495ce95c
jikkyfu <jikkyfu@gmail.com>
2021-02-17 10:50:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,7 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+#fix xray compile Error 2
+rm -rf package/lean/v2ray
+rm -rf package/lean/v2ray-plugin
